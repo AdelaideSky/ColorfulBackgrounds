42b3fe42e4df0021ae7d3e0cded1821b44516676
Adélaïde Sky <augustin.humez@gmail.com>
2023-03-06 22:21:04 +0100

    Initial Commit

---
+++ ColorImageGenerator/ColorImageGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorImageGeneratorApp.swift
+//  ColorImageGenerator
+//
+//  Created by Adélaïde Sky on 06/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorImageGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
